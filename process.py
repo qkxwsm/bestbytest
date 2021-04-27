ea49e339b4c0183621390559afc09d36c4efea2e
qkxwsm <davidhu598@gmail.com>
2021-04-27 12:29:48 -0700

    removed "start" buttons, added tests, updated lambda, added favicon

--- process.py
+++ process.py
@@ -7,14 +7,16 @@ from boto3.dynamodb.conditions import Key
 
 kfactor = decimal.Decimal(20.0)
 default_rtg = decimal.Decimal(1500.0)
-initial = "Start"
+keywords = ["Start", "Loading", "..."]
+
 
 # probability x wins against x + delta
 def prob(delta):
     return decimal.Decimal(1.0 / (1.0 + (math.pow(10.0, float(delta) / (400.0)))))
-    
+
 States = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-        
+#i think it's probably possible to remove this and instead select from DB, might be a way to attempt to save memory though unsure
+
 def response(message, status_code):
     return {
         'statusCode': str(status_code),
@@ -27,7 +29,7 @@ def response(message, status_code):
             "Access-Control-Allow-Credentials" : True
             },
         }
-        
+
 def upd_item(table, name, newrating, result):
     table.update_item(
         Key={"Name":name},
@@ -51,7 +53,7 @@ def upd_item(table, name, newrating, result):
         UpdateExpression="SET #wins = :zero, #losses = :zero",
         ExpressionAttributeNames={'#wins': 'Wins', '#losses': 'Losses'},
         ExpressionAttributeValues={':zero': 0})
-    
+
 def get_item(table, name):
     res = table.query(
         ProjectionExpression="#nm, Rating",
@@ -65,23 +67,29 @@ def get_item(table, name):
             ExpressionAttributeNames={"#nm": "Name"},
             KeyConditionExpression=Key('Name').eq(name))
     return res['Items'][0]
-    
+
 def select_items(names):
     res1 = names[random.randint(0, len(names) - 1)]
     res2 = res1
     while(res2 == res1):
         res2 = names[random.randint(0, len(names) - 1)]
     return res1, res2
-    
+
 def lambda_handler(event, context):
     data = json.loads(event["body"])
     dbname = data["db"]
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(dbname)
     requestid=data["requestid"]
-    if (requestid == 0):
+    if (requestid == 0): #someone wins against someone else
         winner = data["winner"]
         loser = data["loser"]
+        res1 = ""
+        res2 = ""
+        if (dbname == "States"):
+            res1, res2 = select_items(States)
+        if winner in keywords:
+            return response(json.dumps({"choice1": res1, "choice2": res2}), 200)
         winner_rtg = get_item(table, winner)['Rating']
         loser_rtg = get_item(table, loser)['Rating']
         p = prob(winner_rtg - loser_rtg) #1-probability of what happened
@@ -93,21 +101,17 @@ def lambda_handler(event, context):
         upd_item(table, loser, loser_newrtg, 0)
         winner_delta = math.floor(winner_newrtg) - math.floor(winner_rtg)
         loser_delta = math.floor(loser_newrtg) - math.floor(loser_rtg)
-        res1 = ""
-        res2 = ""
-        if (dbname == "States"):
-            res1, res2 = select_items(States)
         return response(json.dumps({
             "prob":1.0-float(p),
             "winner":winner,
             "winner_rtg":float(winner_newrtg),
             "winner_delta":winner_delta,
             "loser":loser,
-            "loser_rtg":float(loser_newrtg), 
+            "loser_rtg":float(loser_newrtg),
             "loser_delta":loser_delta,
             "choice1": res1,
             "choice2": res2}), 200)
-    if (requestid == 1):
+    if (requestid == 1): #requesting leaderboard
         stuff = table.scan()
         items = stuff['Items']
         items.sort(key = lambda cmp:-cmp["Rating"])
@@ -115,5 +119,5 @@ def lambda_handler(event, context):
             i["Rating"] = math.floor(i["Rating"])
             i["Wins"] = int(i["Wins"])
             i["Losses"] = int(i["Losses"])
-        items = [x for x in items if x["Name"] != initial]
+        items = [x for x in items if not x["Name"] in keywords]
         return response(json.dumps(items), 200)
